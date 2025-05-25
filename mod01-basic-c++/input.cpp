047f04ff11f6e7c2d1beb46c7269abc536270015
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-25 14:33:03 +0600

    input in C++, Typecasting

---
+++ mod01-basic-c++/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x;
+    char c = 'a';
+    double d;
+
+    cin >> x >> d;
+
+    cout << x << " " << d << endl;
+
+    cout << (int) c << endl;
+
+    return 0;
+}
