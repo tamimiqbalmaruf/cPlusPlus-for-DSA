df27c27bdf33c1da2b41bc912c2f94b8b59ae926
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-25 12:05:49 +0600

    How to Print in C++

---
+++ mod01-basic-c++/print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+
+    int x = 10;
+
+    char c = 'A';
+
+    double d = 34.55;
+
+    std ::cout << x << " " << c << " " << d << std ::endl;
+
+    std ::cout << "My fav num is " << x << std::endl;
+
+    return 0;
+}
