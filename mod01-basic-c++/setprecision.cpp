e91fe2d3a9b137389b1cd186fe547e27388489f7
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-25 14:43:29 +0600

    EOF and Setprecision in C++

---
+++ mod01-basic-c++/setprecision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+#include <iomanip>;
+using namespace std;
+
+int main()
+{
+    
+    double d = 23.446545;
+
+    cout << fixed << setprecision(2) << d << endl;
+
+    return 0;
+}
