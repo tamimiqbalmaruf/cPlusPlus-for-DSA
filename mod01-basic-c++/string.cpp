a109b0cf3edc612d7a9983b93f28a7a389b25a8b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-26 11:28:08 +0600

    String in C++

---
+++ mod01-basic-c++/string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int x;
+    // cin >> x;
+    // cin.ignore();
+
+    // char s[100];
+    // // cin >> s;
+    // cin.getline(s, 100);
+    // cout << x << endl << s << endl;
+
+
+    int x;
+    string s; // space not working
+
+    cin >> x >> s;
+
+    cout << x << endl << s << endl;
+
+    return 0;
+}
