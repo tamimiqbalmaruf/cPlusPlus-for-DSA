da8636149d610ceb7b1832c529de5463b4ab4c6f
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-26 11:19:50 +0600

    Min, Max, Swap

---
+++ mod01-basic-c++/min_max_swap.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    // if (a < b)
+    // {
+    //     cout << "a is small";
+    // }
+    // else if (b < a)
+    // {
+    //     cout << "b is small";
+    // }
+    // else
+    // {
+    //     cout << "Both are equal";
+    // }
+
+    // cout << min(a, b) << endl;
+    // cout << max(a, b) << endl;
+
+    // cout << min({1,2,3,4,5}) << endl;
+    // cout << max({1,2,3,4,5}) << endl;
+
+    swap(a, b);
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
