5739dbb2a04370ef29be3d0d27c81a9ebc52a3bb
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-31 10:23:53 +0600

    monkey

--- mod4-mid-term/Monkey.cpp
+++ mod4-mid-term/Monkey.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-    
+
+    string s;
+
+    while (getline(cin, s))
+    {
+        string alphabet = "";
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] != ' ')
+            {
+                alphabet += s[i];
+            }
+        }
+
+        sort(alphabet.begin(), alphabet.end());
+
+        cout << alphabet << endl;
+    }
+
     return 0;
 }
