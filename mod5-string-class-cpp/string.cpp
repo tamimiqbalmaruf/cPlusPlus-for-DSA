fa35fa7ff7182b86433af768086e0fb7326b07b5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-01 11:01:11 +0600

    String in C++

---
+++ mod5-string-class-cpp/string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello";
+
+    string s2 = "Hello";
+
+    if (s == s2)
+    {
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not Same" << endl;
+    }
+
+    return 0;
+}
