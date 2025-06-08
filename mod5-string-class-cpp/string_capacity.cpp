4f7e6ef2629a5835e261cace454cb09b51932195
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-08 12:26:38 +0600

    String Capacity Functions

---
+++ mod5-string-class-cpp/string_capacity.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+
+    // cout << s.size() << endl;
+
+    // cout << s.max_size() << endl;
+
+    // s.clear();
+
+    // cout << s.size() << endl;
+
+    s.resize(20, 'y');
+
+
+
+    
+
+    cout << s << endl;
+
+
+
+
+
+
+
+    return 0;
+}
