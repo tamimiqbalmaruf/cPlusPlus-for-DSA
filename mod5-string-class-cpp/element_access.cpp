2f2e5f16738bbce1a0795126b6ccbb3dc3775ba5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-08 16:58:27 +0600

    String Element Access

---
+++ mod5-string-class-cpp/element_access.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << s[0] << endl;
+
+    cout << s.at(2) << endl;
+
+    cout << s.front() << endl;
+    
+    cout << s.back() << endl;
+
+
+    return 0;
+}
