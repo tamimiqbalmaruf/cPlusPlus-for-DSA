e117401cc30438c9a199ba683203b85d585f13ac
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-09 05:11:08 +0600

    String Modifiers Part I

---
+++ mod5-string-class-cpp/String_Modifiers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+
+    string s2 = "Hi";
+
+    // s+=s2;
+
+    // s.append(s2);
+
+    // s.push_back('a');
+
+    s.pop_back();
+
+    cout << s << endl;
+    return 0;
+}
