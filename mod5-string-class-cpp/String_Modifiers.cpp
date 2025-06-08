db90a6224c09f158f190bc10bd7e1083515282fd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-09 05:23:14 +0600

    String Modifiers Part II

--- mod5-string-class-cpp/String_Modifiers.cpp
+++ mod5-string-class-cpp/String_Modifiers.cpp
@@ -13,7 +13,17 @@ int main()
 
     // s.push_back('a');
 
-    s.pop_back();
+    // s.pop_back();
+
+    // s = "Gello";
+
+    // s.assign(s2);
+
+    // s.erase(3, 2);
+
+    // s.replace(5,1, "M");
+
+    s.insert(5, " Moye");
 
     cout << s << endl;
     return 0;
