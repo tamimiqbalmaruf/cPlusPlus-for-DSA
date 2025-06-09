22e2af9b439d0df19819b9367543527ada1adcd5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-09 11:27:13 +0600

    String input with spaces

---
+++ mod5-string-class-cpp/String_input_with_spaces.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    
+    string s;
+    // cin >> s;
+    
+    cin.ignore();
+
+    getline(cin, s);
+    
+
+    cout << x << endl;
+    cout << s << endl;
+    return 0;
+}
