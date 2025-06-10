c168fdec1703b30e4cd06e9bd4bd2e81cbace552
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-10 11:26:58 +0600

    Range based for loop

---
+++ mod6-class-and-string/Range_based_for_loop.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (char c : s)
+    {
+         cout << c << endl;
+    }
+    
+
+    // cout << s << endl;
+    return 0;
+}
