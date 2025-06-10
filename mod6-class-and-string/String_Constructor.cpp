ed0ea610f51172cd4c5d7b4c5f7af72d9fcd5f2e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-10 10:22:59 +0600

    String Constructor

---
+++ mod6-class-and-string/String_Constructor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello";
+
+    // string s("Hello");
+
+    // string s("Hello", 3);
+
+    // string s = "Hello World";
+
+    // string t(s, 4);
+
+    string s(5, 'A');
+
+    cout << s << endl;
+
+
+    return 0;
+}
